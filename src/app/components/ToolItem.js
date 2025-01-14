1f0c608ccd6d171150e56d5c2c7701d9d64810dc
ethan <z82529371@gmail.com>
2025-01-14 16:42:38 +0800

    feat: 新增滑鼠懸停效果，調整拖曳時透明度以提升使用者體驗

--- src/app/components/ToolItem.js
+++ src/app/components/ToolItem.js
@@ -1,5 +1,4 @@
-// ToolItem.js
-import React from "react";
+import React, { useState } from "react";
 import { useDrag, DragPreviewImage } from "react-dnd";
 import { Box } from "@mui/material";
 
@@ -8,6 +7,9 @@ export const ItemTypes = {
 };
 
 const ToolItem = ({ src, hoverSrc, selectedSrc, previewSrc, type, label }) => {
+  // 控制透明度
+  const [isHovered, setIsHovered] = useState(false);
+
   // 用 useDrag 讓此元件可以被拖曳
   const [{ isDragging }, dragRef, preview] = useDrag({
     type: ItemTypes.TOOL, // 必須跟 DropArea 接收的 type 一致
@@ -35,13 +37,15 @@ const ToolItem = ({ src, hoverSrc, selectedSrc, previewSrc, type, label }) => {
           width: 100,
           height: 100,
           marginBottom: 3,
-          opacity: 1,
+          opacity: isDragging ? 0.5 : isHovered ? 0.5 : 1, // 拖曳或 hover 時透明度為 0.5
           cursor: "move",
           backgroundColor: "transparent",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
         }}
+        onMouseEnter={() => setIsHovered(true)} // 滑鼠進入時設定 hover 狀態
+        onMouseLeave={() => setIsHovered(false)} // 滑鼠離開時取消 hover 狀態
       >
         {/* 圖示預覽 */}
         <img src={src} alt={label} width={100} height={100} />
